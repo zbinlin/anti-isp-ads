829a93ab556d595dc7d61d87354c14945a597a59
Zheng Chaoping <zbinlin@gmail.com>
2015-09-20 16:32:40 +0800

    initial

---
+++ anti-isp-ads.meta.js
@@ -0,0 +1,11 @@
+﻿// ==UserScript==
+// @id             anti-isp-ads@zbinlin
+// @name           Anti isp injecting ads
+// @version        1.1
+// @namespace      anti-isp-ads@zbinlin
+// @author         zbinlin
+// @description    anti isp injecting ads
+// @include        http://*
+// @noframes
+// @run-at         document-end
+// ==/UserScript==
---
+++ anti-isp-ads.user.js
@@ -0,0 +1,30 @@
+﻿// ==UserScript==
+// @id             anti-isp-ads@zbinlin
+// @name           Anti isp injecting ads
+// @version        1.1
+// @namespace      anti-isp-ads@zbinlin
+// @author         zbinlin
+// @description    anti isp injecting ads
+// @include        http://*
+// @noframes
+// @run-at         document-end
+// ==/UserScript==
+
+"use strict";
+
+void function () {
+    var title = top.document.title;
+    var iframes = top.document.querySelectorAll("iframe");
+    var vpw = top.document.documentElement.clientWidth;
+    var vph = top.document.documentElement.clientHeight;
+
+    if ("" !== title || !iframes) return;
+
+    for (var i = 0, len = iframes.length; i < len; i++) {
+        var el = iframes[i];
+        if (el.clientWidth === vpw && el.clientHeight === vph) {
+            top.location.reload(true);
+            break;
+        }
+    }
+}();
